929cbf8a3275f3600841459f8216f23752cfd344
johnotander <johnotander@gmail.com>
2013-11-27 12:13:35 -0700

    Add whitelist, nuke projects.

--- app/models/project.rb
+++
@@ -1,8 +0,0 @@
-class Project < ActiveRecord::Base
-  has_many :folders
-  accepts_nested_attributes_for :folders, reject_if: lambda { |a| a[:name].blank? }, allow_destroy: true
-  has_many :scripts, through: :folders
-  belongs_to :user
-
-  attr_accessible :name, :user_id, :folders_attributes
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,16 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   has_many :scripts
-  has_many :projects
-  has_many :folders, through: :projects
   has_many :images, through: :scripts
 
-  has_many :relationships, foreign_key: :follower_id, dependent: :destroy
-  has_many :reverse_relationships, foreign_key: :followed_id, class_name: :Relationship, dependent: :destroy
+  has_many :relationships, 
+           foreign_key: :follower_id, 
+           dependent: :destroy
+
+  has_many :reverse_relationships, 
+           foreign_key: :followed_id, 
+           class_name: :Relationship, 
+           dependent: :destroy
   
   has_many :followers, through: :reverse_relationships, source: :follower
   has_many :followed_users, through: :relationships, source: :followed
