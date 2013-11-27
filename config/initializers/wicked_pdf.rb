ff926b53d9d8f275e0d3d84ab3a0fc916716ae83
John Otander <johnotander@gmail.com>
2013-11-27 14:08:34 -0700

    Check the env correctly.

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,4 @@
-path_to_wkhtmltopdf = Env.production? ?
+path_to_wkhtmltopdf = Rails.env.production? ?
   Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s :
   '/Users/johno/.rvm/gems/ruby-2.0.0-p353/bin/wkhtmltopdf'
 
