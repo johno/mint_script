e441ea22ed982f36b904b10eca5f7a823b5b1ccd
John Otander <johnotander@gmail.com>
2013-11-27 17:29:05 -0700

    Break out some buttons into a helper.

--- app/helpers/scripts_helper.rb
+++ app/helpers/scripts_helper.rb
@@ -1,5 +1,29 @@
 module ScriptsHelper
 
+  def edit_script_button(script)
+    link_to '<i class="icon-edit"></i>'.html_safe, edit_script_path(script), class: 'pure-button'
+  end
+
+  def pdf_script_button(script)
+    link_to '<i class="icon-file"></i>'.html_safe, 
+            script_path(script, format: :pdf), 
+            class: 'pure-button'
+  end
+
+  def delete_script_button(script)
+    link_to '<i class="icon-trash"></i>'.html_safe, 
+            script, 
+            method: :delete, 
+            data: { confirm: 'Are you sure?' }, 
+            class: 'pure-button'
+  end
+
+  def create_script_button
+    link_to '<i class="icon-plus"></i> &nbsp; Create a New Script'.html_safe, 
+             new_script_path, 
+             class: 'pure-button'
+  end
+
   def edit_script_title(script)
     form_for(script, remote: true) do |f|
       f.text_field :title, placeholder: 'No Title Given', class: :script_title_edit
